3be0c803b7f820089b7c7ac409390bc5c869916d
AdamRomek <adam@kliny.com>
2020-06-24 12:38:20 +0200

    2020-06-24 godz.12.37 ukrywanie buttona

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
  //$_SESSION['nazwa'] = wartosc;
  session_start();
 $_SESSION['index'] == 1;
@@ -20,7 +20,7 @@ echo "Sesja ;" .$_SESSION['nr_faktury'];
 	}
 
  // zmienna potrzebna do prawidlowego wczytania includowanych plikow
-$add_site = true; 
+$add_site = true;
 
 ?>
 <!DOCTYPE html>
@@ -82,35 +82,35 @@ $add_site = true;
     <td class="tg-0lax"><input id="cena_brutto" type="text" name="cena_brutto" /></td>
     <td class="tg-0lax"><input id="kodK" type="text" name="kodK" /></td>
     <td class="tg-0lax"><input id="data_zak" type="text" name="data_zak" /></td>
-    
-    
+
+
   </tr>
   <input class="btn" type="reset" value="Wyczyść rekord" ></form>
-</form> 
+</form>
 </table> -->
 
 <div id="form">
-	 	<form action="zapiszTowar.php" method="post" enctype="multipart/form-data"> 
-	 	
+	 	<form action="zapiszTowar.php" method="post" enctype="multipart/form-data">
+
 
  			<div id="faktura">
 <fieldset>
 	<legend>Faktura i dostawca</legend>
  	 	   <label class="lab" for="nr_faktury">Nr faktury</label>
  	      <input id="nr_faktury" type="text" name="nr_faktury" value="<?php echo ($_SESSION['nr_faktury'])?>" />
- 	              
+
          <label class="lab" for="id_f">Indeks faktury.   (może być nr faktury)</label>
          <input id="id_f" type="text" name="id_f" value="<?php echo ($_SESSION['id_f']) ?>"  />
-	     
+
 	      <label class="lab" for="data_zak">Data zakupu</label>
 		   <input id="data_zak" type="date" name="data_zak" value="<?php echo ($_SESSION['data_zak']) ?>"   />
-        
+
          <label class="lab" for="nazwa_dost">Nazwa dostawcy</label>
   		   <input id="nazwa1" type="text" name="nazwa_dost" value="<?php echo ($_SESSION['nazwa_dost']) ?>"  />
-  		   
+
   		   <label class="lab" for="indeks_dost">Indeks dostawcy</label>
          <input id="indeksD" type="text" name="indeks_dost" value="<?php echo ($_SESSION['indeks_dost']) ?>"  /><br>
-</fieldset> 		  
+</fieldset>
  		 	</div>
 <br>
  		 	<div id="przerwa"></div>
@@ -126,40 +126,51 @@ $add_site = true;
 
 			<label class="lab" for="Jm">Jm </label>
 			<input id="jm" type="text" name="jm" value="szt" ><br>
-			
+
 			<label for="ilosc">Ilość</label>&nbsp;&nbsp;
 			<input id="ilosc" type="text" name="ilosc" value="<?php echo ($_GET['ilosc']) ?>"  required >
-			
-			
+
+
 			<label class="lab">Cena netto wyliczana z brutto</label>
-	<input type="text" name="bruttoZb" size="10" onkeyup="licz_Z_brutto(this.form)"> VAT lub RABAT 
-	<input type="text" name="vatZb" size="5" onkeyup="licz_Z_brutto(this.form)"> 
-				
+	<input type="text" name="bruttoZb" size="10" onkeyup="licz_Z_brutto(this.form)"> VAT lub RABAT
+	<input type="text" name="vatZb" size="5" onkeyup="licz_Z_brutto(this.form)">
+
 			<label class="lab">Cena netto</label>
 			<input id="cena" type="text" name="cena_z" value="<?php echo ($_GET['cena_z']) ?> "  required >
-			
-			
+
+
 
 			<label class="lab" for="pkwiu">PKWiU</label>
 			<input id="pkwiu" type="text" name="pkwiu" value="<?php echo ($_GET['pkwiu']) ?>"  >
-			
+
 			<label class="lab" for="vat">VAT</label>
 			<input id="vat" type="text" name="vat" value="<?php echo ($_GET['vat']) ?>"  required ><br>
 
 			<label class="lab" for="kod_k">Kod kreskowy</label>
 			<input id="kodKT" type="text" name="kod_k" value="<?php echo ($_GET['kod_k']) ?>"  >
-			
-			<label class="lab" for="cena_brutto">Cena sprzedaży brutto</label> 
+
+			<label class="lab" for="cena_brutto">Cena sprzedaży brutto</label>
 			<input type="text" id="cena_brutto" name="cena_brutto" value="<?php echo ($_GET['cena_brutto']) ?>"  required  >
 </fieldset>
-        			<div id="button">               
-						<button>Dodaj nowy towar...</button>
+        			<div id="przycisk">
+						<button id="button">Dodaj nowy towar...</button>
 						&nbsp;&nbsp;&nbsp;&nbsp;<input class="btn" type="reset" value="Wyczyść rekord" >
-					</div> 
-					
+						
+<?php
+if($_SESSION['nr_faktury'] ){
+	$button = "block";
+}
+else
+{
+	$button = "none";
+}
+?>
+					</div>
+				
 	</div>
-	
-	</form> 
+
+
+	</form>
 
 </div>
 
@@ -180,9 +191,9 @@ $(function() {
 	               $("#ilosc").val(ui.item.ilosc);
 	               $("#cena").val(ui.item.cena);
 	               $("#cena_brutto").val(ui.item.cena_brutto);
-	               $("#pkwiu").val(ui.item.pkwiu);  
+	               $("#pkwiu").val(ui.item.pkwiu);
 	               $("#kodKT").val(ui.item.kodK);
-	               $("#data_zak").val(ui.item.dataZak); 
+	               $("#data_zak").val(ui.item.dataZak);
                   return false;
          },
             select: function(event, ui) {
@@ -193,10 +204,10 @@ $(function() {
                  $("#cena").val(ui.item.cena);
                  $("#ilosc").val(ui.item.ilosc);
                  $("#cena_brutto").val(ui.item.cena_brutto);
-                 $("#pkwiu").val(ui.item.pkwiu);  
+                 $("#pkwiu").val(ui.item.pkwiu);
                  $("#kodKT").val(ui.item.kodKT);
                  $("#data_zak").val(ui.item.dataZak);
-								// alert("Selected: " + ui.item.value + " aka " + ui.item.label); 
+								// alert("Selected: " + ui.item.value + " aka " + ui.item.label);
                 return false;
             }
     });
@@ -216,7 +227,7 @@ $(function() {
 	               $("#cena_brutto").val(ui.item.cena_brutto);
 	               $("#pkwiu").val(ui.item.pkwiu); */
 	               $("#kodKT").val(ui.item.kodKT);
-	               //$("#stan_mag").val(ui.item.stan);  
+	               //$("#stan_mag").val(ui.item.stan);
                   return false;
          },
             select: function(event, ui) {
@@ -233,7 +244,7 @@ $(function() {
                 return false;
             }
     });
-});   
+});
 
 $(function() {
     $("#kodKT").autocomplete({
@@ -247,9 +258,9 @@ $(function() {
 	               $("#ilosc").val(ui.item.ilosc);
 	               $("#cena").val(ui.item.cena);
 	               $("#cena_brutto").val(ui.item.cena_brutto);
-	               $("#pkwiu").val(ui.item.pkwiu); 
+	               $("#pkwiu").val(ui.item.pkwiu);
 	               $("#kodKT").val(ui.item.kodKT);
-	             //  $("#stan_mag").val(ui.item.stan);  
+	             //  $("#stan_mag").val(ui.item.stan);
                   return false;
          },
             select: function(event, ui) {
@@ -263,7 +274,7 @@ $(function() {
                  $("#pkwiu").val(ui.item.pkwiu);
                  $("#kodKT").val(ui.item.kodKT);
                //   $("#stan_mag").val(ui.item.stan);
-					//			 alert("Selected: " + ui.item.value + " aka " + ui.item.label); 
+					//			 alert("Selected: " + ui.item.value + " aka " + ui.item.label);
                 return false;
             }
     });
@@ -288,6 +299,10 @@ $(function() {
             }
     });
 });
+$(document).ready(function () {
+document.getElementById('button').style.display = "<?php print $button; ?>";	
+})
+
 </script>
 <script src="script/funkcje.js" ></script>
 </body>
